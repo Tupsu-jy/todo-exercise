33b21deb574fe8af8ef63f7ade757e11d6780a17
Tupsu-jy <ylinenjaakko@gmail.com>
2025-09-17 17:18:26 +0300

    deploying added to jenkins pipeline

--- gcp-deploy.sh
+++ gcp-deploy.sh
@@ -12,12 +12,11 @@ echo "Deploying backend..."
 cd api
 docker build -t gcr.io/${PROJECT_ID}/php-app:${VERSION} .
 docker push gcr.io/${PROJECT_ID}/php-app:${VERSION}
-#gcloud run deploy php-app \
-#  --image gcr.io/${PROJECT_ID}/php-app:latest \
-#  --platform managed \
-#  --region ${REGION} \
-#  --allow-unauthenticated \
-#  --port 8000
+gcloud run deploy php-app \
+  --image gcr.io/${PROJECT_ID}/php-app:${VERSION} \
+  --platform managed \
+  --region ${REGION} \
+  --allow-unauthenticated \
 
 echo "Deploying frontend..."
 cd ../frontend
@@ -25,10 +24,10 @@ docker build -t gcr.io/${PROJECT_ID}/flutter-app:${VERSION} \
   --build-arg API_URL=https${BACKEND_URL}/api \
   --build-arg WS_URL=wss${BACKEND_URL}/app/ob0ildef0rapadlha0hl .
 docker push gcr.io/${PROJECT_ID}/flutter-app:${VERSION}
-#gcloud run deploy flutter-app \
-#  --image gcr.io/${PROJECT_ID}/flutter-app:latest \
-#  --platform managed \
-#  --region ${REGION} \
-#  --allow-unauthenticated
+gcloud run deploy flutter-app \
+ --image gcr.io/${PROJECT_ID}/flutter-app:${VERSION} \
+ --platform managed \
+ --region ${REGION} \
+ --allow-unauthenticated
 
 echo "Done!"
